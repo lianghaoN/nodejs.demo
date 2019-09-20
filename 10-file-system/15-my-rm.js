aae82da2b9b040243ba4431833430f281650c5d1
lianghao <597868880@qq.com>
2019-09-20 16:44:17 +0800

    node

--- 10-file-system/15-my-rm.js
+++ 10-file-system/15-my-rm.js
@@ -1,10 +1,36 @@
 #!/usr/bin/node
 
 const fs  = require('fs'),
+      join = require('path').join,
       file = process.argv[2];
 
-try{
-  
-}catch(e){
-
+if(typeof(file)==='undefined'){
+  console.error('请指定要删除的文件名或者目录名！');
+  process.exit(1);
 }
+
+if(!fs.existsSync(file)){
+  console.error('%s not exist!', file);
+  process.exit(2);
+}
+
+if(fs.statSync(src).isFile())  fs.unlinkSync(file);
+
+if(fs.statSync(src).isDirectory()) deleteDir(file);
+
+function deleteDir(folder) {
+  var files = fs.readdirSync(folder);
+
+  for(var i=0; i<files.length; i++) {
+    var file = join(folder, files[i]);
+
+    if(fs.statSync(file).isFile()) {
+      fs.unlinkSync(file);
+      continue;
+    }
+
+    if(fs.statSync(file).isDirectory()) deleteDir(file);
+  }
+
+  fs.rmdirSync(folder);
+} 
